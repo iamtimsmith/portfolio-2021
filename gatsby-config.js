20192c74bee6a8915350076548f1aad5383b2d15
Tim Smith <tgsmith489@gmail.com>
2021-06-22 01:21:46 -0500

    Added rss feed to site.

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,6 +97,63 @@ module.exports = {
         fetchOptions: {},
       },
     },
+		{
+      resolve: `gatsby-plugin-feed-mdx`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  url: `${site.siteMetadata.siteUrl}${node.fields.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}${node.fields.slug}`,
+                  custom_elements: [{ "content:encoded": node.html }]
+                });
+              });
+            },
+            query: `
+						{
+							allMdx(
+								filter: {fileAbsolutePath:{regex:"//blog/./i"}, frontmatter:{published:{eq:true}}}
+								sort: { order: DESC, fields: [fileAbsolutePath] },
+							) {
+								nodes {
+									excerpt
+									html
+									fields {
+										slug
+									}
+									frontmatter {
+										title
+									}
+								}
+							}
+						}
+            `,
+            output: "/rss.xml",
+            title: "Tim Smith's RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/blog/"
+          }
+        ]
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
